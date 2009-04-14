7843c1b9c265241f4f04b44f99b9b05892b110e0
Geoff Smith <github@geoff.me>
2009-04-14 15:55:45 +0100

    Fixed up the gem

--- lib/ruby-farm-capistrano.rb
+++ lib/ruby-farm-capistrano.rb
@@ -1,6 +1,10 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+def initialize_ruby_farm_capistrano() 
+  load(File.dirname(__FILE__) + '/ruby-farm-capistrano/d50_capistrano.rb')
+end
+
 module RubyFarmCapistrano
   VERSION = '0.0.1'
 end
