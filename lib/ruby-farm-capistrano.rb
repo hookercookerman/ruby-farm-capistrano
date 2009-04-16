5a98261d2c62f24645c496bbebd333d4db731fec
Geoff Smith <github@geoff.me>
2009-04-16 10:39:20 +0100

    Updated the initialization function for new names

--- lib/ruby-farm-capistrano.rb
+++ lib/ruby-farm-capistrano.rb
@@ -2,7 +2,7 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 def initialize_ruby_farm_capistrano() 
-  load(File.dirname(__FILE__) + '/ruby-farm-capistrano/d50_capistrano.rb')
+  load(File.dirname(__FILE__) + '/ruby-farm-capistrano/farm_capistrano.rb')
 end
 
 module RubyFarmCapistrano
